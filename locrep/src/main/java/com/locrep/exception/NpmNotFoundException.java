518dffe2232cbbaa84163e2a0a52ef43161fc2c2
muratungor <muratungor@gmail.com>
2019-03-09 15:25:46 +0300

    murat ungor

---
+++ locrep/src/main/java/com/locrep/exception/NpmNotFoundException.java
@@ -0,0 +1,13 @@
+package com.locrep.exception;
+
+public class NpmNotFoundException extends RuntimeException {
+
+	public NpmNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+
+}
