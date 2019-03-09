941f794817143ae10c04256db714b5641454bb8a
bugragoksu <bugragoksu33@hotmail.com>
2019-03-10 02:35:24 +0300

    urlresolver

--- src/main/java/com/locrep/controller/FileController.java
+++ src/main/java/com/locrep/controller/FileController.java
@@ -7,7 +7,9 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 
@@ -15,41 +17,41 @@ import java.io.IOException;
 @RequestMapping("/")
 public class FileController {
 
-//    @RequestMapping(value = "/sid", method = RequestMethod.GET,
-//            produces = MediaType.IMAGE_JPEG_VALUE)
-//
-//    public void getImage(HttpServletResponse response) throws IOException {
-//
-//        var imgFile = new ClassPathResource("image/sid.jpg");
-//
-//        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-//        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-//    }
+    // @RequestMapping(value = "/sid", method = RequestMethod.GET,
+    // produces = MediaType.IMAGE_JPEG_VALUE)
+    //
+    // public void getImage(HttpServletResponse response) throws IOException {
+    //
+    // var imgFile = new ClassPathResource("image/sid.jpg");
+    //
+    // response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+    // StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+    // }
 
-//    @RequestMapping(value = "/sid", method = RequestMethod.GET,
-//            produces = MediaType.IMAGE_JPEG_VALUE)
-//    public ResponseEntity<byte[]> getImage() throws IOException {
-//
-//        ClassPathResource imgFile = new ClassPathResource("image/sid.jpg");
-//        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
-//
-//        return ResponseEntity
-//                .ok()
-//                .contentType(MediaType.IMAGE_JPEG)
-//                .body(bytes);
-//    }
+    // @RequestMapping(value = "/sid", method = RequestMethod.GET,
+    // produces = MediaType.IMAGE_JPEG_VALUE)
+    // public ResponseEntity<byte[]> getImage() throws IOException {
+    //
+    // ClassPathResource imgFile = new ClassPathResource("image/sid.jpg");
+    // byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+    //
+    // return ResponseEntity
+    // .ok()
+    // .contentType(MediaType.IMAGE_JPEG)
+    // .body(bytes);
+    // }
 
     @RequestMapping(value = "/id")
-    String getIdByValue(){
+    String getIdByValue() {
         System.out.println("ID is ");
         return "Get ID from query string of URL with value element";
     }
 
     @ResponseBody
-   @RequestMapping(value = "/maven2/{groupId}/{artifactId}/{version}")
-   public String getId(@PathVariable("groupId") String groupId, @PathVariable("artifactId") String artifactId,
-           @PathVariable("version") String version) {
+    @RequestMapping(value = "/maven2/{groupId}/{artifactId}/{version}")
+    public String getId(@PathVariable("groupId") String groupId, @PathVariable("artifactId") String artifactId,
+            @PathVariable("version") String version) {
 
-       return groupId+"."+artifactId+"."+version;
-   }
+        return groupId + "." + artifactId + "." + version;
+    }
 }
