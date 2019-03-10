09b108329a09212284d44fac6c76bed7fef94ecf
bugragoksu <bugragoksu33@hotmail.com>
2019-03-10 05:40:57 +0300

    getpath

--- src/main/java/com/locrep/controller/FileController.java
+++ src/main/java/com/locrep/controller/FileController.java
@@ -2,7 +2,6 @@ package com.locrep.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
