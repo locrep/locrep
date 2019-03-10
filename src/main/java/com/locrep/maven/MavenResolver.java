0343f3a168db4b300f4270593019937ff6417851
muratungor <muratungor@gmail.com>
2019-03-10 10:51:43 +0300

    filepath

--- src/main/java/com/locrep/maven/MavenResolver.java
+++ src/main/java/com/locrep/maven/MavenResolver.java
@@ -26,7 +26,7 @@ public class MavenResolver
 
     private static String baseUrl = "https://repo.maven.apache.org";
 
-    private static String filePath = "./mavenArtifacts";
+    private static String filePath = "./mavenArtifacts/";
 
     public void IterateDependencies(Artifact artif)
     {
